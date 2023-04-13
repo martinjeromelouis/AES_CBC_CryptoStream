a0cb7dc86790d014a545be26b152f15ac1db5d41
martinjeromelouis <130647418+martinjeromelouis@users.noreply.github.com>
2023-04-13 10:31:27 +0200

    Create deps.ts

---
+++ lib/deps.ts
@@ -0,0 +1,7 @@
+export { FixedLengthUint8ArrayTransformStream } from "https://deno.land/x/fixed_length_uint8array_transform_stream@v1.0.6/mod.ts";
+export { Aes } from "https://deno.land/x/crypto@v0.10.0/aes.ts";
+export {
+  Cbc,
+  Padding,
+} from "https://deno.land/x/crypto@v0.10.0/block-modes.ts";
+export { concat } from "std/bytes/mod.ts";
